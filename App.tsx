9fd8671ab75afccd9968d668c34529eb042e89ef
LeoTan <dextertan1981@gmail.com>
2025-11-28 16:52:57 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '48px',
+      fontFamily: 'sans-serif'
+    }}>
+      Hello World
+    </div>
+  );
+};
+
+export default App;
